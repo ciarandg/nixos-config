4bf97bb9f00f20a54ccb5bc6b28e8227fe01ba99
Ciaran De Groot <ciaran.deg@gmail.com>
2022-06-04 10:48:44 -0600

    Add hme config for sparrow

---
+++ home/configurations/sparrow/ciaran.nix
@@ -0,0 +1,22 @@
+{
+  home-manager,
+  secrets,
+  ...
+}: username: system:
+home-manager.lib.homeManagerConfiguration {
+  configuration = {
+    nixpkgs.config.allowUnfreePredicate = pkg: true;
+    home.username = username;
+    home.homeDirectory = "/home/${username}";
+    imports = [
+      ../../modules/shells
+      ../../modules/applications/cli
+    ];
+  };
+  inherit system username;
+  homeDirectory = "/home/${username}";
+  stateVersion = "21.03";
+  extraSpecialArgs = {
+    inherit secrets;
+  };
+}
