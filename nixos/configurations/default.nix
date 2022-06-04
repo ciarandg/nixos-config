ffd86c7db9b7da1013f4da034ee29ea87828f51b
Ciaran De Groot <ciaran.deg@gmail.com>
2022-06-03 20:30:14 -0600

    Inline nixos system configs

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,4 +1,33 @@
-{self, ...} @ inputs: {
-  ivy = import ./ivy inputs;
-  slate = import ./slate inputs;
+{
+  self,
+  nixpkgs,
+  nixos-hardware,
+  ...
+} @ inputs: let
+  mkSystem = {
+    system,
+    specialArgs,
+    modules,
+  }:
+    nixpkgs.lib.nixosSystem {
+      inherit system specialArgs modules;
+    };
+in {
+  ivy = mkSystem {
+    system = "x86_64-linux";
+    specialArgs = inputs;
+    modules = [
+      ./ivy/configuration.nix
+      ./ivy/hardware-configuration.nix
+      nixos-hardware.nixosModules.lenovo-thinkpad-x1-extreme-gen2
+    ];
+  };
+  slate = mkSystem {
+    system = "aarch64-linux";
+    specialArgs = inputs;
+    modules = [
+      ./slate/configuration.nix
+      ./slate/hardware-configuration.nix
+    ];
+  };
 }
